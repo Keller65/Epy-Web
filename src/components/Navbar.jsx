560f56f275266a45a66ff7478c8da226e11456a2
Keller65 <107809849+Keller65@users.noreply.github.com>
2024-03-18 13:16:57 -0600

    Se soluciono el problema del carrito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export function Navbar() {
 
     return (
         <React.Fragment>
-            <nav className="navbar">
+            <nav className="navbar backdrop-blur-[5px]">
                 <Link to="/Home" className='icon' onClick={() => navigate('/Home')}>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill={fillHome ? 'black' : 'white'} stroke="black" strokeWidth="2" d="M3 12.1836C3 10.1235 3 9.0939 3.468 8.2407C3.9342 7.3866 4.7883 6.8574 6.4956 5.7972L8.2956 4.6803C10.1001 3.5598 11.0028 3 12 3C12.9972 3 13.899 3.5598 15.7044 4.6803L17.5044 5.7972C19.2117 6.8574 20.0658 7.3866 20.5329 8.2407C21 9.0939 21 10.1235 21 12.1827V13.5525C21 17.0625 21 18.8184 19.9452 19.9092C18.8913 21 17.1939 21 13.8 21H10.2C6.8061 21 5.1087 21 4.0548 19.9092C3 18.8184 3 17.0634 3 13.5525V12.1836Z" />
@@ -57,30 +57,27 @@ export function Navbar() {
                 </Link>
 
                 <div className='icon' onClick={openModal}>
-                    <img src="assets/bolsa.svg" alt="" className='icon-img' />
+                    <img src="assets/bolsa.svg" alt="" className='h-[25px] w-[25px] cursor-pointer' />
                 </div>
 
                 <Link to="/Profile" className='icon' onClick={() => navigate('/Profile')}>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M17.982 18.725C17.2833 17.7999 16.3793 17.0496 15.3412 16.5334C14.3031 16.0171 13.1594 15.749 12 15.75C10.8407 15.749 9.6969 16.0171 8.65883 16.5334C7.62077 17.0496 6.71675 17.7999 6.01801 18.725M17.982 18.725C19.3455 17.5122 20.3071 15.9136 20.7412 14.1411C21.1753 12.3686 21.0603 10.5061 20.4115 8.80049C19.7627 7.09488 18.6107 5.62679 17.1084 4.59091C15.6061 3.55503 13.8244 3.00031 11.9995 3.00031C10.1747 3.00031 8.39295 3.55503 6.89062 4.59091C5.38829 5.62679 4.23634 7.09488 3.58755 8.80049C2.93875 10.5061 2.82376 12.3686 3.25783 14.1411C3.6919 15.9136 4.65451 17.5122 6.01801 18.725M17.982 18.725C16.336 20.1932 14.2056 21.0032 12 21C9.79404 21.0034 7.66425 20.1934 6.01801 18.725M15 9.75001C15 10.5457 14.6839 11.3087 14.1213 11.8713C13.5587 12.4339 12.7957 12.75 12 12.75C11.2044 12.75 10.4413 12.4339 9.87869 11.8713C9.31608 11.3087 9.00001 10.5457 9.00001 9.75001C9.00001 8.95436 9.31608 8.1913 9.87869 7.62869C10.4413 7.06608 11.2044 6.75001 12 6.75001C12.7957 6.75001 13.5587 7.06608 14.1213 7.62869C14.6839 8.1913 15 8.95436 15 9.75001Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                        <path d="M11.5 7L9.5 8L9 10.5L10 11.5L11 12H13.5L15 10L13.5 7.5L11.5 7Z" fill={fillProfile ? 'black' : ''} />
-                        <circle cx="12" cy="12" r="9.15" stroke="black" strokeWidth="1.7" />
-                        <path d="M12 15C8.5 15 6.55455 17.1091 5.5 18.1636C6.20303 19.0424 8.10902 20.8 12.3272 20.8C17.0727 20.8 17.7941 18.6909 18.3213 18.1636C17.5 17 16 15 12 15Z" fill={fillProfile ? 'black' : ''} strokeWidth="0.3" />
+                    <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20.0483 8.19612C20.2928 8.54618 20.5068 8.91654 20.6881 9.30315L23.3186 10.7656C23.647 12.2368 23.6505 13.7618 23.3287 15.2344L20.6881 16.6969C20.5068 17.0835 20.2928 17.4539 20.0483 17.8039L20.099 20.8203C18.9842 21.8362 17.6648 22.6015 16.2295 23.0649L13.6397 21.511C13.2136 21.5414 12.786 21.5414 12.36 21.511L9.78028 23.0547C8.34044 22.5998 7.01666 21.8374 5.9006 20.8203L5.95138 17.8141C5.70892 17.4592 5.49497 17.0856 5.31153 16.6969L2.68107 15.2344C2.35261 13.7633 2.34915 12.2382 2.67091 10.7656L5.31153 9.30315C5.49281 8.91654 5.70687 8.54618 5.95138 8.19612L5.9006 5.17971C7.0154 4.16385 8.33483 3.39851 9.77013 2.93518L12.36 4.48909C12.786 4.45861 13.2136 4.45861 13.6397 4.48909L16.2193 2.94534C17.6592 3.40023 18.983 4.16262 20.099 5.17971L20.0483 8.19612Z" fill={fillProfile ? 'black' : 'transparent'} stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M13 17.875C15.6924 17.875 17.875 15.6924 17.875 13C17.875 10.3076 15.6924 8.125 13 8.125C10.3076 8.125 8.125 10.3076 8.125 13C8.125 15.6924 10.3076 17.875 13 17.875Z" stroke={fillProfile ? 'white' : 'black'} fill={fillProfile ? 'black' : 'transparent'} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                 </Link>
-
-                {modalOpen && (
-                    <div className="modal">
-                        <button onClick={closeModal} className='btnModal-close'>
-                            <X stroke='gray' size={20} className='icon-img' />
-                        </button>
-
-                        <Carrito />
-                    </div>
-                )}
-
                 <Outlet />
             </nav>
+
+            {modalOpen && (
+                <div className="modal">
+                    <button onClick={closeModal} className='btnModal-close'>
+                        <X stroke='gray' size={20} className='icon-img' />
+                    </button>
+
+                    <Carrito />
+                </div>
+            )}
         </React.Fragment>
     );
 }
